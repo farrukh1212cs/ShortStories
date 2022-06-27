059796a2027226220912db2e1e8875f97cf6a9ac
Farrukh Rehman <farrukh1212cs@outlook.com>
2022-06-27 18:03:07 +0500

    first check in stories api

---
+++ WebAPI/Controllers/StoriesController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using BOL;
+using DAL;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StoriesController : ControllerBase
+    {
+        private readonly SSDbContext _context;
+
+        public StoriesController(SSDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Stories
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Story>>> GetStories()
+        {
+          if (_context.Stories == null)
+          {
+              return NotFound();
+          }
+            return await _context.Stories.ToListAsync();
+        }
+
+        // GET: api/Stories/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Story>> GetStory(int id)
+        {
+          if (_context.Stories == null)
+          {
+              return NotFound();
+          }
+            var story = await _context.Stories.FindAsync(id);
+
+            if (story == null)
+            {
+                return NotFound();
+            }
+
+            return story;
+        }
+
+        // PUT: api/Stories/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutStory(int id, Story story)
+        {
+            if (id != story.SSId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(story).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!StoryExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Stories
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Story>> PostStory(Story story)
+        {
+          if (_context.Stories == null)
+          {
+              return Problem("Entity set 'SSDbContext.Stories'  is null.");
+          }
+            _context.Stories.Add(story);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetStory", new { id = story.SSId }, story);
+        }
+
+        // DELETE: api/Stories/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteStory(int id)
+        {
+            if (_context.Stories == null)
+            {
+                return NotFound();
+            }
+            var story = await _context.Stories.FindAsync(id);
+            if (story == null)
+            {
+                return NotFound();
+            }
+
+            _context.Stories.Remove(story);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool StoryExists(int id)
+        {
+            return (_context.Stories?.Any(e => e.SSId == id)).GetValueOrDefault();
+        }
+    }
+}
--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,6 +1,17 @@
+using DAL;
+
 var builder = WebApplication.CreateBuilder(args);
+//Step 1: Add Ref Of BOL and DAL
+
+//Step 2: Add Services
+builder.Services.AddControllers();
+builder.Services.AddDbContext<SSDbContext>();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+//Step 3: Add mapControllers
+app.MapControllers();
 
 app.Run();
+//Step 4: Add Pacakage
+//Microsoft.VisualStudio.Web.CodeGeneration.Design
