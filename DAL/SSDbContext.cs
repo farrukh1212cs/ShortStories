3a7ce3a049a55c887e22982030570409731e8787
Farrukh Rehman <farrukh1212cs@outlook.com>
2022-06-27 17:51:24 +0500

    Add project files.

---
+++ DAL/SSDbContext.cs
@@ -0,0 +1,16 @@
+﻿using BOL;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL
+{
+    public class SSDbContext : IdentityDbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer(@"Server=.;Database=SSDb_8;Trusted_Connection=True;");
+        }
+        public DbSet<Story>? Stories { get; set; }
+    }
+}
