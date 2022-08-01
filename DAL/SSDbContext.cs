0a29a3e64ce1286ed34abc49874d8b9f28006963
Farrukh Rehman <farrukh1212cs@outlook.com>
2022-08-01 18:52:34 +0500

    check in working on lecture 9

--- DAL/SSDbContext.cs
+++ DAL/SSDbContext.cs
@@ -6,11 +6,11 @@ namespace DAL
 {
     public class SSDbContext : IdentityDbContext
     {
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            base.OnConfiguring(optionsBuilder);
-            optionsBuilder.UseSqlServer(@"Server=.;Database=SSDb_8;Trusted_Connection=True;");
-        }
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    base.OnConfiguring(optionsBuilder);
+        //    optionsBuilder.UseSqlServer(@"Server=.;Database=SSDb_8;Trusted_Connection=True;");
+        //}
         public DbSet<Story>? Stories { get; set; }
         public DbSet<Category>? Categories { get; set; }
     }
